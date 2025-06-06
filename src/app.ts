0989b9d7ddd9ebada090107e34f037816e6ba517
suraj <4347494+surajgoraya@users.noreply.github.com>
2025-06-05 22:01:00 -0400

    feat: ✨ Update Express, add colourful logs + a new error handler (#5)
    * feat(logging): :lipstick: add colours to logs, make them cleaner
    
    * chore: Update Express to LTS
    
    * chore: :art: add tsconfig, prettier, and lint code
    
    * fix: add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,77 @@
-import express from "express";
-import helmet from "helmet";
-import path from "path";
-import { getHealthInfo, getRandomGIF } from "./lib/services";
-import { templates } from "./lib/templates";
-import { processEnvironmentConfig } from "./lib/configuration";
+import express from 'express';
+import helmet from 'helmet';
+import path from 'path';
+
+import { processEnvironmentConfig } from './lib/configuration';
+import { logger } from './lib/logger';
+import { getHealthInfo, getRandomGIF } from './lib/services';
+import { templates } from './lib/templates';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 if (process.env.IS_PROXIED) {
-  app.enable("trust proxy");
+	app.enable('trust proxy');
 }
 
 app.use(
-  helmet({
-    crossOriginResourcePolicy: {
-      policy: processEnvironmentConfig({ config: process.env.CORS_POLICY }),
-    },
-  })
+	helmet({
+		crossOriginResourcePolicy: {
+			policy: processEnvironmentConfig({ config: process.env.CORS_POLICY }),
+		},
+	}),
 );
 
 //static directory to serve our favicon
-app.use(express.static(path.join(process.cwd(), "src", "assets")));
+app.use(express.static(path.join(process.cwd(), 'src', 'assets')));
 
 /**
  * Middleware to tell google to not index anything returned.
  */
 app.use((req, res, next) => {
-  res.setHeader("X-Robots-Tag", "noindex");
-  next();
+	res.setHeader('X-Robots-Tag', 'noindex');
+	next();
 });
 
-app.get("/", async (req, res) => {
-  const randomGIF = await getRandomGIF();
+app.get('/', async (req, res) => {
+	const randomGIF = await getRandomGIF();
 
-  if (req.query.key && req.query.key === process.env.KEY) {
-    if (randomGIF !== undefined) {
-      res.setHeader("Cache-Control", "no-cache");
-      res.setHeader("Expires", "0");
+	if (req.query.key && req.query.key === process.env.KEY) {
+		if (randomGIF !== undefined) {
+			res.setHeader('Cache-Control', 'no-cache');
+			res.setHeader('Expires', '0');
 
-      res.sendFile(randomGIF);
-    } else {
-      console.error("[Kirlia Error]:", "Unable to get random GIF.");
-      res.status(500).send(templates.errors.serverError);
-    }
-  } else {
-    console.warn(
-      "[Kirlia Warn]:",
-      "Unauthorized access. Requesting IP: ",
-      req.ips.length ? req.ips : req.ip
-    );
-    res.status(401).send(templates.errors.notAuthorized);
-  }
+			res.sendFile(randomGIF);
+		} else {
+			logger.error('Unable to get random GIF. More than likely the ./static folder is missing.');
+			res.status(500).send(templates.errors.serverError);
+		}
+	} else {
+		logger.warn('Unauthorized access. Requesting IP: ', req.ips.length ? req.ips : req.ip);
+		res.status(401).send(templates.errors.notAuthorized);
+	}
 });
 
-app.get("/health", (req, res) => {
-  res.status(200).json(getHealthInfo());
+app.get('/health', (req, res) => {
+	res.status(200).json(getHealthInfo());
 });
 
 //catch all route for 404s
 app.use((req, res, next) => {
-  res.status(404).send(templates.errors.notFound);
+	res.status(404).send(templates.errors.notFound);
+});
+
+//@ts-ignore
+app.use((err, req, res, next) => {
+	logger.error(
+		`Uncaught Error! In ${req.url} - Serving 503.\n\n\t Please report this in GitHub issues. Error below:\n\t`,
+		err,
+	);
+	res.status(503).send(templates.errors.serviceUnavailable);
 });
 
 app.listen(port, () => {
-  console.info(
-    `Kirlia is listening on ${port} - Online as of ${new Date().toLocaleDateString()} @ ${new Date().toLocaleTimeString()}`
-  );
+	logger.info(
+		`Kirlia is listening on port ${port} - Online as of ${new Date().toLocaleDateString()} @ ${new Date().toLocaleTimeString()}`,
+	);
 });
--- src/lib/templates.ts
+++ src/lib/templates.ts
@@ -1,22 +1,27 @@
 const templates = {
-  errors: {
-    notAuthorized:`<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
+	errors: {
+		notAuthorized: `<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
                   <img src="/kirlia-logo-dark.png" style="margin: 0 0 0 0;"/>
                   <h1>Unauthorized.</h1>
                   <p>Please provide a key when accessing GIF endpoint.</p>
                 </html>`,
 
-    serverError:`<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
+		serverError: `<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
                   <img src="/kirlia-logo-dark.png" style="margin: 0 0 0 0;"/>
                   <h1>Something has gone terribly wrong.</h1>
                   <p>Something went wrong when rendering the GIF. Ensure that <code>/static/</code> folder has GIFs.</p>
                 </html>`,
-    notFound: `<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
+		serviceUnavailable: `<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
+                  <img src="/kirlia-logo-dark.png" style="margin: 0 0 0 0;"/>
+                  <h1>Service Unavailable.</h1>
+                  <p>Woah! Looks like Kirlia is asleep. Please try again later once she's awake!</p>
+                </html>`,
+		notFound: `<html style="font-family: sans-serif; margin: auto; padding: 2rem 2rem;">
                   <img src="/kirlia-logo-dark.png" style="margin: 0 0 0 0;"/>
                   <h1>Not Found.</h1>
                   <p>The page you're requesting could not be found. Sorry!</p>
                 </html>`,
-  },
+	},
 };
 
 export { templates };
